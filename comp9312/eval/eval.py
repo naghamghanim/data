90a29c064770008cda32011d50043c67e59516fc
naghamghanim <56317559+naghamghanim@users.noreply.github.com>
2022-11-24 08:13:49 +0200

    fsdfd

--- comp9312/eval/eval.py
+++ comp9312/eval/eval.py
@@ -114,7 +114,7 @@ def main(args):
         shuffle=False,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
-        collate_fn=dataset.collate_fn,
+        collate_fn=dataset[0].collate_fn,
     )
 
     # Initialize the model
